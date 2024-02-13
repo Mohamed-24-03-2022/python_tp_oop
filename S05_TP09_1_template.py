9f00717989e1dbd21024e50e969bfa0f2972e976
Mohamed <mohakatalonya10@gmail.com>
2024-02-13 13:51:05 +0100

    added tp9_1 professor correction

--- S05_TP09_1_template.py
+++ S05_TP09_1_template.py
@@ -12,12 +12,12 @@ class Grid:
                 - columns_count' : initialisé avec le nombre de colonnes de 'grid_init'."""
         self.__grid = grid_init
         self.__lines_count = len(grid_init)
-        self.__columns_count = len(grid_init[0])
+        self.__columns_count = len(grid_init[0]) if len(grid_init) else 0
 
     def get_grid(self):
         return self.__grid
 
-    def det_lines_count(self):
+    def get_lines_count(self):
         return self.__lines_count
 
     def get_columns_count(self):
@@ -25,8 +25,9 @@ class Grid:
 
     def fill_random(self, values):
         """ Rempli la grille de valeurs aléatoires de la liste 'values'"""
-        self.__grid = [[random.choice(values) for _ in range(
-            self.__columns_count)] for _ in range(self.__lines_count)]
+        self.__grid = [[random.choice(values)
+                        for _ in range(self.__columns_count)]
+                       for _ in range(self.__lines_count)]
 
     def get_line(self, line_number):
         """ Extrait la ligne numéro 'line_number' de la grille."""
@@ -34,163 +35,73 @@ class Grid:
 
     def get_column(self, column_number):
         """ Extrait la colonne numéro 'column_number' de la grille."""
-        res = []
-        for line in range(len(self.__grid)):
-            for col in range(len(self.__grid[line])):
-                if (col == column_number):
-                    res += [self.__grid[line][col]]
-        return res
+        return [line[column_number] for line in self.__grid]
 
     def get_diagonal(self):
         """ Extrait la diagonale de la grille."""
-        res = []
-        for line in range(len(self.__grid)):
-            for col in range(len(self.__grid[line])):
-                if (line == col):
-                    res.append(self.__grid[line][col])
-
-        return res
+        diagonal_size = min(self.__lines_count, self.__columns_count)
+        return [self.__grid[line_number][line_number] for line_number in range(diagonal_size)]
 
     def get_anti_diagonal(self):
         """ Extrait l'antidiagonale de la grille."""
-        res = []
-        line_init = 0
-        grid_init = len(self.__grid[0]) - 1
-
-        for line in range(len(self.__grid)):
-            for col in range(len(self.__grid[line])):
-                if (line_init > line):
-                    break
-                res.append(self.__grid[line_init][grid_init])
-                line_init += 1
-                grid_init -= 1
-
-        return res
+        diagonal_size = min(self.__lines_count, self.__columns_count)
+        return [self.__grid[line_number][self.__columns_count - line_number - 1]
+                for line_number in range(diagonal_size)]
 
     def get_line_str(self, line_number, separator='\t'):
         """ Retourne la chaine de caractère correspondant à la concaténation des valeurs de la ligne numéro
         'line_number' de la grille. Les caractères sont séparés par le caractère 'separator'."""
-        string = ''
-        for i, value in enumerate(self.get_line(line_number)):
-            if i+1 == len(self.__grid[line_number]):
-                string += f'{value}'
-            else:
-                string += f'{value}{separator}'
-        return string
+        return separator.join(str(value) for value in self.__grid[line_number])
 
     def get_grid_str(self, separator='\t'):
         """ Retourne la chaine de caractère représentant la grille.
                 Les caractères de chaque ligne sont séparés par le caractère 'separator'.
                 Les lignes sont séparées par le caractère de retour à la ligne '\n'."""
-        string = ''
-
-        for line in range(len(self.__grid)):
-            for value in self.get_line(line):
-                string += f'{value}'
-
-            if line+1 < len(self.__grid):
-                string += '\n'
-            else:
-                string += ''
-
-        return string
+        return '\n'.join(self.get_line_str(line_number, separator) for line_number in range(self.__lines_count))
 
     def has_equal_values(self, value):
         """ Teste si toutes les valeurs de la grille sont égales à 'value'."""
-        for line in range(len(self.__grid)):
-            for col in range(len(self.__grid[line])):
-                if (self.__grid[line][col] != value):
-                    return False
-
-        return True
+        return all([all([value == grid_value for grid_value in line]) for line in self.__grid])
 
     def is_square(self):
         """ Teste si la grille a le même nombre de lignes et de colonnes."""
-        lines = 0
-        cols = 0
-        for line in range(len(self.__grid)):
-            for col in range(len(self.__grid[line])):
-                cols += 1
-            lines += 1
-
-        return True if lines == (cols / lines) else False
+        return self.__lines_count == self.__columns_count
 
     def get_count(self, value):
         """ Compte le nombre d'occurrences de 'value' dans la grille."""
-        count = 0
-        for line in range(len(self.__grid)):
-            for col in range(len(self.__grid[line])):
-                if (self.__grid[line][col] == value):
-                    count += 1
-
-        return count
+        return sum(line.count(value) for line in self.__grid)
 
     def get_sum(self):
         """ Fait la somme de tous les éléments de la grille."""
-        sum = 0
-
-        for line in range(len(self.__grid)):
-            for col in range(len(self.__grid[line])):
-                sum += self.__grid[line][col]
-
-        return sum
+        return sum(sum(line) for line in self.__grid)
 
     def get_coordinates_from_cell_number(self, cell_number):
         """ Converti un numéro de case 'cell_number' de la grille vers les coordonnées (ligne, colonne)
         correspondants."""
-        line = 0
-        i = 0
-        for l in range(len(self.__grid)):
-            col = 0
-            for _ in range(len(self.__grid[l])):
-                if i == cell_number:
-                    break
-                col += 1
-                i += 1
-            if i == cell_number:
-                break
-        line += 1
-
-        return (line, col)
+        return cell_number // self.__columns_count, cell_number % self.__columns_count
 
     def get_cell_number_from_coordinates(self, line_number, column_number):
         """ Converti les coordonnées ('line_number', 'column_number') de la grille vers le numéro de case
         correspondant."""
-        line = 0
-        cell_number = 0
-        for l in range(len(self.__grid)):
-            col = 0
-            for _ in range(len(self.__grid[l])):
-                col += 1
-                cell_number += 1
-                if (line_number == line and column_number == col):
-                    return cell_number
-            line += 1
-
-        return None
+        return line_number * self.__columns_count + column_number
 
     def get_cell(self, cell_number):
         """ Extrait la valeur de la grille en position 'cell_number'."""
-        line, col = self.get_coordinates_from_cell_number(cell_number)
-
-        return self.__grid[line][col]
+        line_number, column_number = self.get_coordinates_from_cell_number(
+            cell_number)
+        return self.__grid[line_number][column_number]
 
     def set_cell(self, cell_number, value):
         """ Positionne la valeur 'value' dans la case 'cell_number' de la grille."""
-        line, col = self.get_coordinates_from_cell_number(cell_number)
-        self.__grid[line][col] = value
+        line_number, column_number = self.get_coordinates_from_cell_number(
+            cell_number)
+        self.__grid[line_number][column_number] = value
 
     def get_same_value_cell_numbers(self, value):
         """ Fourni la liste des numéros des cases à valeur égale à 'value' dans la grille."""
-        list = []
-        i = 0
-        for line in range(len(self.__grid)):
-            for col in range(len(self.__grid[line])):
-                if (self.__grid[line][col] == value):
-                    list.append(i)
-                i += 1
-
-        return list
+        return [cell_number
+                for cell_number in range(self.__lines_count * self.__columns_count)
+                if self.get_cell(cell_number) == value]
 
     def get_neighbour(self, line_number, column_number, delta, is_tore=True):
         """ Retourne le voisin de la cellule ('line_number', 'column_number') de la grille. La définition de voisin
@@ -198,31 +109,48 @@ class Grid:
         voisine est alors (ligne + delta_ligne, colonne + delta_colonne).
                 Si 'is_tore' est à 'True' le voisin existe toujours en considérant la grille comme un tore.
                 Si 'is_tore' est à 'False' retourne 'None' lorsque le voisin est hors de la grille."""
-        grid = self.__grid
-
-        if (is_tore):
-            if (column_number + delta[1] <= len(grid[0]) - 1):
-                return grid[line_number + delta[0]][column_number + delta[1]]
-            elif (column_number + delta[1] > len(grid[0]) - 1):
-                return grid[line_number + delta[0]][0]
-
-        elif (not is_tore):
-            if (column_number + delta[1] <= len(grid[0]) - 1):
-                return grid[line_number + delta[0]][column_number + delta[1]]
-            elif (column_number + delta[1] > len(grid[0]) - 1):
-                return None
+        new_line_number, new_column_number = line_number + \
+            delta[0], column_number + delta[1]
+        if is_tore or 0 <= new_line_number < self.__lines_count and 0 <= new_column_number < self.__columns_count:
+            return self.__grid[new_line_number % self.__lines_count][new_column_number % self.__columns_count]
+        return None
 
     def get_neighborhood(self, line_number, column_number, deltas, is_tore=True):
         """ Retourne la liste des N voisins de la position ('lins_number', 'column_number') dans la grille correspondant
          aux N 2-uplet (delta_ligne, delta_colonne) fournis par la liste deltas.
                 Si 'is_tore' est à 'True' le voisin existe toujours en considérant la grille comme un tore.
                 Si 'is_tore' est à 'False' un voisin hors de la grille n'est pas considéré."""
-        neighborhood = []
-        for delta in deltas:
-            neighborhood.append(self.get_neighbour(
-                line_number, column_number, delta, is_tore))
+        return [self.get_neighbour(line_number, column_number, delta, is_tore)
+                for delta in deltas]
 
-        return neighborhood
+    def get_cell_neighbour_number(self, cell_number, delta, is_tore=True):
+        """ Retourne le numéro de cellule voisine de la cellule 'cell_number' de la grille. La définition de
+        voisin correspond à la distance positionnelle indiquée par le 2-uplet 'delta' = (delta_ligne, delta_colonne).
+        La case voisine est alors (ligne + delta_ligne, colonne + delta_colonne).
+                Si 'is_tore' est à 'True' le voisin existe toujours en considérant la grille comme un tore.
+                Si 'is_tore' est à 'False' retourne 'None' lorsque le voisin est hors de la grille."""
+        line_number, column_number = self.get_coordinates_from_cell_number(
+            cell_number)
+        line_number, column_number = line_number + \
+            delta[0], column_number + delta[1]
+        if is_tore or 0 <= line_number < self.__lines_count and 0 <= column_number < self.__columns_count:
+            line_number %= self.__lines_count
+            column_number %= self.__columns_count
+            return self.get_cell_number_from_coordinates(line_number, column_number)
+        return None
+
+    def get_cell_neighborhood_numbers(self, cell_number, deltas, is_tore=True):
+        """ Retourne la liste des N cellules voisines de la position 'cell_number'
+        dans la grille correspondant aux N 2-uplet (delta_ligne, delta_colonne) fournis par la liste deltas.
+                Si 'is_tore' est à 'True' le voisin existe toujours en considérant la grille comme un tore.
+                Si 'is_tore' est à 'False' un voisin hors de la grille n'est pas considéré."""
+        res = []
+        for delta in deltas:
+            neighbour = self.get_cell_neighbour_number(
+                cell_number, delta, is_tore)
+            if neighbour is not None:
+                res.append(neighbour)
+        return sorted(res)
 
 
 if __name__ == '__main__':
@@ -240,6 +168,7 @@ if __name__ == '__main__':
     # Constantes de test
     LINES_COUNT_TEST, COLUMNS_COUNT_TEST = 5, 7
     LINE_NUMBER_TEST, COLUMN_NUMBER_TEST = 1, 6
+    CELL_NUMBER_TEST = 13
     VALUE_TEST = 0
     VALUES_TEST = list(range(2))
     IS_TORE_TEST = True
@@ -282,4 +211,12 @@ if __name__ == '__main__':
         LINE_NUMBER_TEST, COLUMN_NUMBER_TEST, WIND_ROSE, IS_TORE_TEST) == [0, 1, 1, 1, 0, 1, 1, 1]
     assert GRID_TEST.get_neighborhood(LINE_NUMBER_TEST, COLUMN_NUMBER_TEST, WIND_ROSE, not IS_TORE_TEST) == [
         0, None, None, None, 0, 1, 1, 1]
+    assert GRID_TEST.get_cell_neighbour_number(
+        CELL_NUMBER_TEST, DIRECTION_TEST, IS_TORE_TEST) == 7
+    assert not GRID_TEST.get_cell_neighbour_number(
+        CELL_NUMBER_TEST, DIRECTION_TEST, not IS_TORE_TEST)
+    assert GRID_TEST.get_cell_neighborhood_numbers(
+        CELL_NUMBER_TEST, WIND_ROSE, IS_TORE_TEST) == [0, 5, 6, 7, 12, 14, 19, 20]
+    assert GRID_TEST.get_cell_neighborhood_numbers(
+        CELL_NUMBER_TEST, WIND_ROSE, not IS_TORE_TEST) == [5, 6, 12, 19, 20]
     print("Tests all OK")
