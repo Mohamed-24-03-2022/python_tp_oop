4809cf788b927b9ac97ece44b0c5918dd5b31afa
Mohamed Bouzentouta <bouzent231@C304L-138C08.campus.unicaen.fr>
2024-03-04 15:45:18 +0100

    finished tp13_1

--- S07_TP13_01.py
+++ S07_TP13_01.py
@@ -1,18 +1,30 @@
 
 # __str__ = __repr__
 # __radd__ = __add__
+
 import tkinter as tk
 
 
-def leave(app):
-    app.quit()
-    app.destroy()
-
-
 if __name__ == "__main__":
+    TITLE = "Test tkinter"
+    WIDTH = 800
+    HEIGHT = 500
+    POS_x = 100
+    POS_Y = 50
     myApp = tk.Tk()
-    myApp.title("My first GUI")
-    myApp.geometry("900x200")
-    tk.Button(myApp, text="Click me", command=myApp.quit).pack()
-    tk.Button(myApp, text="Destroy me", command=myApp.destroy).pack()
+    myApp.title(TITLE)
+    myApp.geometry(f"{WIDTH}x{HEIGHT}+{POS_x}+{POS_Y}")
+
+    # create a frame
+    my_frame = tk.Frame(myApp)
+    my_frame.pack()
+    # create a Canvas to draw
+    canvas = tk.Canvas(my_frame, background='white')
+    canvas.pack()
+
+    # crate an exit button
+    button = tk.Button(myApp, text="Click me", command=myApp.quit)
+    button.pack(side=tk.RIGHT)
+
+    # lunch the app
     myApp.mainloop()
