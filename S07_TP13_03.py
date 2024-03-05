052efe8172bdcd01d7f58e779e17e96c7aa58ad8
Mohamed <mohakatalonya10@gmail.com>
2024-03-05 15:36:42 +0100

    Finished tp13_3 and half through tp14_1

--- S07_TP13_03.py
+++ S07_TP13_03.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class MyApp(tk.Tk):
     __COLORS = {
         "cell_background": "white",
@@ -20,19 +21,48 @@ class MyApp(tk.Tk):
         self.__f_main = tk.Frame(self)
         self.__f_main.pack()
 
-        self.__c_draw = tk.Canvas(self.__f_main, background='white')
+        width = grid.get_columns_count()*(cell_size+gutter_size) + margin_size * 2
+        # height = grid.get_lines_count()*(cell_size+gutter_size) + margin_size * 2
+        self.__c_draw = tk.Canvas(
+            self.__f_main, background='white', width=width, height=600)
         self.__c_draw.pack()
 
-        self.__b_quit = tk.Button(self, text="Close", command=self.quit)
-        self.__b_quit.pack(side=tk.RIGHT)
+        self.__b_quit = tk.Button(
+            self, text="Close", command=self.quit, fg=self.__COLORS["widget_text"], )
+        self.__b_quit.pack(side=tk.BOTTOM)
 
-        # self.draw_grid()
+        self.draw_grid()
 
-        
-        
-    
-    def draw_grid(self, grid_lines = True, content=True):
-        pass
+    def draw_grid(self, grid_lines=True, content=True):
+        x0 = self.__margin_size
+        y0 = self.__margin_size
+
+        x1 = self.__margin_size + self.__cell_size
+        y1 = self.__margin_size + self.__cell_size
+
+        x_center = (x0 + x1) / 2
+        y_center = (y0 + y1) / 2
+
+        if (grid_lines):
+            for i in range(self.__grid.get_lines_count()):
+                for j in range(self.__grid.get_columns_count()):
+                    self.__c_draw.create_rectangle(
+                        x0, y0, x1, y1, fill=self.__COLORS["cell_background"])
+
+                    if (content):
+                        self.__c_draw.create_text(
+                            x_center, y_center, text=self.__grid.get_cell(i+j), fill=self.__COLORS["grid_text"])
+
+                    x0 += self.__cell_size + self.__gutter_size
+                    x1 += self.__cell_size + self.__gutter_size
+                    x_center = (x0 + x1) / 2
+
+                x0 = self.__margin_size
+                x1 = self.__margin_size + self.__cell_size
+                y0 += self.__cell_size + self.__gutter_size
+                y1 += self.__cell_size + self.__gutter_size
+                x_center = (x0 + x1) / 2
+                y_center = (y0 + y1) / 2
 
 
 if __name__ == "__main__":
@@ -42,7 +72,9 @@ if __name__ == "__main__":
     COLUMNS_COUNT = 30
     GRID_TEST = Grid([[0] * COLUMNS_COUNT for _ in range(LINES_COUNT)])
     GRID_TEST.fill_random(range(10000))
-
-    myApp = MyApp(GRID_TEST)
+    CELL_SIZE = 50
+    GUTTER_SIZE = 5
+    MARGIN_SIZE = 10
+    myApp = MyApp(GRID_TEST, CELL_SIZE, GUTTER_SIZE, MARGIN_SIZE)
     myApp.title("Mon application d'affichage d'une grille")
     myApp.mainloop()
---
+++ S07_TP14_01.py
@@ -0,0 +1,98 @@
+import random
+
+
+class Element:
+    def __init__(self, char_repr):
+        self.__char_repr = char_repr
+
+    def __repr__(self):
+        return self.__char_repr
+
+    def __eq__(self, other):
+        return other.__repr__() == self.__repr__() and issubclass(other.__class__, Element)
+
+
+class Ground(Element):
+    def __init__(self):
+        Element.__init__(self, '\u2B1C')
+
+
+class Resource(Element):
+    def __init__(self, char_repr, value):
+        Element.__init__(self, char_repr)
+        self.__value = value
+
+    def get_value(self):
+        return self.__value
+
+
+class Water(Resource):
+    def __init__(self):
+        Resource.__init__(self, '\U0001F41F', 1)
+
+
+class Herb(Resource):
+    def __init__(self):
+        Resource.__init__(self, '\U0001F33F', 2)
+
+
+class Animal(Element):
+    def __init__(self, char_repr, life_max):
+        Element.__init__(self, char_repr)
+        self.__life_max = life_max
+        self.__age = 0
+        self.__gender = random.choice([0, 1])  # 0 for Female, 1 for Masculine
+        self.__bar_life = [0, life_max]
+
+        d1, d2 = random.choices([-1, 0, 1], k=2)
+        while (d1 == 0 and d2 == 0):
+            d1, d2 = random.choices([-1, 0, 1], k=2)
+
+        self.__current_direction = [d1, d2]
+
+    def get_age(self):
+        return self.__age
+
+    def ageing(self):
+        self.__age += 1
+
+    def get_gender(self):
+        return self.__gender
+
+    def get_life_max(self):
+        return self.__life_max
+
+    def get_life(self):
+        return self.__bar_life[0]
+
+    def is_alive(self):
+        return self.__bar_life[0] > 0
+
+    def is_dead(self):
+        return not self.__is_alive()
+
+    def recovering_life(self, value):
+        self.__bar_life[0] = min(self.__bar_life[0] + value, self.__life_max)
+
+    def losing_life(self, value):
+        self.__bar_life[0] = max(0, self.__bar_life[0] - value)
+
+    def get_current_direction(self):
+        return self.__current_direction
+
+    def set_current_direction(self, line_direction, column_direction):
+        self.__current_direction = [line_direction, column_direction]
+
+
+if (__name__ == "__main__"):
+    print(Ground(), str(Ground()))
+    print(Ground() == str(Ground()))
+    print(Ground() == Ground())
+    print(Ground() is Ground())
+
+    TYPES_COUNT = {Herb: 2, Water: 3,  Cow: 2, Dragon: 1, Lion: 5, Mouse: 10}
+    ELEMENTS_BY_TYPE = {element_type: [element_type() for _ in range(element_count)]
+                        for element_type, element_count in TYPES_COUNT.items()}
+
+    for element_type, elements in ELEMENTS_BY_TYPE.items():
+        print(f"{element_type.__name__}: {elements}")
